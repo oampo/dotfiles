2f944e81d2a5c7a5d1ccd3ee3b711479415590c5
Joe Turner <joe@oampo.co.uk>
2014-04-18 10:41:58 +0000

    Update install script and fix tmux config for nitrous.

--- install.sh
+++ install.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-dir=~/Code/dotfiles # dotfiles directory
-olddir=~/Code/dotfiles.bak # old dotfiles backup directory
+dir=$(cd `dirname $0` && pwd) # dotfiles directory
+cd -
 files="vimrc tmux.conf gitignore" # list of files/folders to symlink in homedir
 
 ##########
+echo $dir;
 
-# create dotfiles_old
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-echo "Moving any existing dotfiles from ~ to $olddir"
+cd ~
 for file in $files; do
-    mv ~/.$file $olddir
+    rm -f .$file
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/.$file .$file
 done
 
+mkdir -p ~/.vim/bundle
+if [ ! -d ~/.vim/bundle/vundle ]; then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
 vim +PluginInstall +qall
